a8f549d9489ce1751ddf21d7450bc03d35503d92
Nathalia Cardozo <nathalia.cardozo@alura.com.br>
2023-08-22 11:43:22 -0300

    first commit

---
+++ src/componentes/ListaSuspensa/index.tsx
@@ -0,0 +1,29 @@
+import './ListaSuspensa.css'
+
+interface ListaSuspensaProps {
+    label: string
+    obrigatorio: boolean
+    valor: string
+    aoAlterado: (valor: string) => void
+    itens: string[]
+}
+
+const ListaSuspensa = ({ aoAlterado, itens, label, obrigatorio, valor } : ListaSuspensaProps) => {
+    return (
+        <div className='lista-suspensa'>
+            <label>{label}</label>
+            <select 
+                onChange={evento => aoAlterado(evento.target.value)} 
+                required={obrigatorio} 
+                value={valor}
+            >
+                <option value=""></option>
+                {itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
